bcb72bb345e628065320b452ed39f59750cef92c
FattyMieo <fong2009@live.com.my>
2018-05-11 16:12:01 +0800

    Edit build.sh

--- build.sh
+++ build.sh
@@ -12,9 +12,6 @@ echo "Attempting to build $project for Windows"
   -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
   -quit
 
-echo 'Logs from build (Windows)'
-cat $(pwd)/unity.log
-
 echo "Attempting to build $project for OS X"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
@@ -24,9 +21,6 @@ echo "Attempting to build $project for OS X"
   -projectPath $(pwd) \
   -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
   -quit
-
-echo 'Logs from build (OS X)'
-cat $(pwd)/unity.log
 
 echo "Attempting to build $project for Linux"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
@@ -38,11 +32,11 @@ echo "Attempting to build $project for Linux"
   -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
   -quit
 
-echo 'Logs from build (Linux)'
+echo 'Logs from build'
 cat $(pwd)/unity.log
 
 
-echo 'Attempting to zip builds'
-zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
-zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
-zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
+# echo 'Attempting to zip builds'
+# zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
+# zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
+# zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
