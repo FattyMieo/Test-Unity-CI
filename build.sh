989c3f7fb89c5f8c9dd964367120ca1ccf55756b
FattyMieo <fong2009@live.com.my>
2018-05-11 15:39:41 +0800

    Edit Travis

--- build.sh
+++ build.sh
@@ -12,6 +12,9 @@ echo "Attempting to build $project for Windows"
   -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" 
   -quit
 
+echo 'Logs from build (Windows)'
+cat $(pwd)/unity.log
+
 echo "Attempting to build $project for OS X"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity 
   -batchmode 
@@ -21,6 +24,9 @@ echo "Attempting to build $project for OS X"
   -projectPath $(pwd) 
   -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" 
   -quit
+
+echo 'Logs from build (OS X)'
+cat $(pwd)/unity.log
 
 echo "Attempting to build $project for Linux"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity 
@@ -32,7 +38,7 @@ echo "Attempting to build $project for Linux"
   -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" 
   -quit
 
-echo 'Logs from build'
+echo 'Logs from build (Linux)'
 cat $(pwd)/unity.log
 
 
