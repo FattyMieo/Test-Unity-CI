5eb963764625d3fcd5e4b05ec93777e9429d5d7b
FattyMieo <fong2009@live.com.my>
2018-05-11 16:40:18 +0800

    Set target to Windows

--- build.sh
+++ build.sh
@@ -12,26 +12,6 @@ echo "Attempting to build $project for Windows"
   -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
   -quit
 
-echo "Attempting to build $project for OS X"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
-  -quit
-
-echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
-  -quit
-
 echo " "
 echo "-----------------"
 echo " Logs from build "
@@ -44,7 +24,51 @@ echo "       END       "
 echo "-----------------"
 echo " "
 
+#echo "Attempting to build $project for OS X"
+#/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+#  -batchmode \
+#  -nographics \
+#  -silent-crashes \
+#  -logFile $(pwd)/unity.log \
+#  -projectPath $(pwd) \
+#  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
+#  -quit
+#
+#echo " "
+#echo "-----------------"
+#echo " Logs from build "
+#echo "-----------------"
+#echo " "
+#cat $(pwd)/unity.log
+#echo " "
+#echo "-----------------"
+#echo "       END       "
+#echo "-----------------"
+#echo " "
+#
+#echo "Attempting to build $project for Linux"
+#/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+#  -batchmode \
+#  -nographics \
+#  -silent-crashes \
+#  -logFile $(pwd)/unity.log \
+#  -projectPath $(pwd) \
+#  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
+#  -quit
+#
+#echo " "
+#echo "-----------------"
+#echo " Logs from build "
+#echo "-----------------"
+#echo " "
+#cat $(pwd)/unity.log
+#echo " "
+#echo "-----------------"
+#echo "       END       "
+#echo "-----------------"
+#echo " "
+
 echo "Attempting to zip builds"
-zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
-zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
+#zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
+#zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
 zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
